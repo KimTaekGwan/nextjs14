ce52356e2b95647a85210c5cb8be9a0f08beaabe
TaekGwan Kim <sbe07032@naver.com>
2024-03-16 21:24:49 +0900

    Add Google Third-Parties & Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css"
 import AuthButton from "../components/AuthButton"
 import DeployButton from "../components/DeployButton"
 import { createClient } from "@/supabase/server"
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google"
+import { ThemeProvider } from "@/providers/theme-provider"
+import { ThemeButton } from "@/components/ThemeButton"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -34,29 +37,39 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
-            {isSupabaseConnected && <AuthButton />}
-          </div>
-        </nav>
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-        <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-        </footer>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+              <DeployButton />
+              <ThemeButton />
+              {isSupabaseConnected && <AuthButton />}
+            </div>
+          </nav>
+          <main className="min-h-screen flex flex-col items-center">
+            {children}
+          </main>
+          <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+            <p>
+              Powered by{" "}
+              <a
+                href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                target="_blank"
+                className="font-bold hover:underline"
+                rel="noreferrer"
+              >
+                Supabase
+              </a>
+            </p>
+          </footer>
+        </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
+      <GoogleTagManager gtmId={process.env.GOOGLE_TAGMANAGER_ID || ""} />
     </html>
   )
 }
