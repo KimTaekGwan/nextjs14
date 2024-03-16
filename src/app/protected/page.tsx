734262e72daa041e91597d44f1a32e2ab3e73c41
TaekGwan Kim <sbe07032@naver.com>
2024-03-16 15:45:21 +0900

    add directory 'src'

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,37 @@
+import DeployButton from "@/src/components/DeployButton"
+import AuthButton from "@/src/components/AuthButton"
+import { createClient } from "@/src/supabase/server"
+import Header from "@/src/components/Header"
+import { redirect } from "next/navigation"
+
+export default async function ProtectedPage() {
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    return redirect("/login")
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="w-full">
+        <div className="py-6 font-bold bg-purple-950 text-center">
+          This is a protected page that you can only see as an authenticated
+          user
+        </div>
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <DeployButton />
+            <AuthButton />
+          </div>
+        </nav>
+      </div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  )
+}
