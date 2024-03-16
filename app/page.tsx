73d577a975754b22974c6ab8a9675be0b8b960c2
TaekGwan Kim <sbe07032@naver.com>
2024-03-16 15:35:08 +0900

    add shadcn/ui ui % color

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import DeployButton from "../components/DeployButton"
 import AuthButton from "../components/AuthButton"
 import { createClient } from "@/utils/supabase/server"
 // import Header from "@/components/Header"
+import { Button } from "@/components/ui/button"
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -25,6 +26,31 @@ export default async function Index() {
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
+      <div>
+        <section id="about" className="container py-24 sm:py-32">
+          <div className="bg-muted/50 border rounded-lg py-12">
+            <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+              <div className="bg-green-0 flex flex-col justify-between">
+                <div className="pb-6">
+                  <h2 className="text-3xl md:text-4xl font-bold">
+                    <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+                      About{" "}
+                    </span>
+                    Company
+                  </h2>
+                  <p className="text-xl text-muted-foreground mt-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
