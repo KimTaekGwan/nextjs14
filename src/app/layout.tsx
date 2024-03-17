0484f3da2fe527058f9bc07708a7d31d163234c0
TaekGwan Kim <sbe07032@naver.com>
2024-03-17 22:29:45 +0900

    init velite

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import { GeistSans } from "geist/font/sans"
 import "./globals.css"
-import AuthButton from "../components/AuthButton"
-import DeployButton from "../components/DeployButton"
+import AuthButton from "@/components/AuthButton"
+import DeployButton from "@/components/DeployButton"
 import { createClient } from "@/supabase/server"
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google"
 import { ThemeProvider } from "@/providers/theme-provider"
 import { ThemeButton } from "@/components/ThemeButton"
+import { cn } from "@/lib/utils"
+import { Inter } from "next/font/google"
+
+import { SiteHeader } from "@/components/site-header"
+import { Providers } from "@/components/providers"
+import { siteConfig } from "@/config/site"
+import { SiteFooter } from "@/components/site-footer"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -16,6 +23,8 @@ export const metadata = {
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
 }
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 export default function RootLayout({
   children,
@@ -36,37 +45,19 @@ export default function RootLayout({
   const isSupabaseConnected = canInitSupabaseClient()
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-              <DeployButton />
-              <ThemeButton />
-              {isSupabaseConnected && <AuthButton />}
-            </div>
-          </nav>
-          <main className="min-h-screen flex flex-col items-center">
-            {children}
-          </main>
-          <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-            <p>
-              Powered by{" "}
-              <a
-                href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                target="_blank"
-                className="font-bold hover:underline"
-                rel="noreferrer"
-              >
-                Supabase
-              </a>
-            </p>
-          </footer>
-        </ThemeProvider>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased text-foreground",
+          inter.variable
+        )}
+      >
+        <Providers>
+          <div className="relative flex min-h-dvh flex-col bg-background">
+            <SiteHeader />
+            <main className="flex-1">{children}</main>
+            <SiteFooter />
+          </div>
+        </Providers>
       </body>
       <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
       <GoogleTagManager gtmId={process.env.GOOGLE_TAGMANAGER_ID || ""} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,64 @@
 import CTASection from "@/components/CTASection"
-import About from "../components/About"
+import About from "@/components/About"
+import { buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site"
+import { cn, sortPosts } from "@/lib/utils"
+import Link from "next/link"
+import { posts } from "#site/content"
+import { PostItem } from "@/components/post-item"
 
 export default async function Index() {
+  const latestPosts = sortPosts(posts).slice(0, 5)
   return (
     // <div className="flex-1 w-full flex flex-col gap-20 items-center">
     <div>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+        <div className="container flex flex-col gap-4 text-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+            Hello, I&apos;m James
+          </h1>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            Welcome to my blog template. Built using tailwind, shadcn, velite
+            and Nextjs 14.
+          </p>
+          <div className="flex flex-col gap-4 justify-center sm:flex-row">
+            <Link
+              href="/blog"
+              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
+            >
+              View my blog
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              className={cn(
+                buttonVariants({ variant: "outline", size: "lg" }),
+                "w-full sm:w-fit"
+              )}
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2>
+        <ul className="flex flex-col">
+          {latestPosts.map((post) => (
+            <li key={post.slug} className="first:border-t first:border-border">
+              <PostItem
+                slug={post.slug}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
       <About />
       <CTASection />
     </div>
